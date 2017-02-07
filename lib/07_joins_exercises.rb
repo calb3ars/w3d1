4445a89c39cf7e8a220da943d92df0a99a238de2
Andrew Jiang <andrew@Andrews-MacBook-Pro.local>
2017-02-06 17:39:53 -0800

    Completes 07

--- lib/07_joins_exercises.rb
+++ lib/07_joins_exercises.rb
@@ -197,11 +197,49 @@ def films_by_cast_size
   # List the films released in the year 1978 ordered by the number of actors
   # in the cast (descending), then by title (ascending).
   execute(<<-SQL)
+    SELECT
+      title, COUNT(name)
+    FROM
+      movies
+    JOIN castings
+      ON movies.id = movie_id
+    JOIN actors
+      ON actor_id = actors.id
+    WHERE
+      yr = 1978
+    GROUP BY
+      title
+
+    ORDER BY
+      COUNT(name) DESC,
+      title ASC
+
   SQL
 end
 
 def colleagues_of_garfunkel
   # List all the people who have played alongside 'Art Garfunkel'.
   execute(<<-SQL)
+  SELECT
+    name
+  FROM movies
+  JOIN castings
+    ON movies.id = movie_id
+  JOIN actors
+    ON actor_id = actors.id
+  WHERE
+    title IN (
+      SELECT
+        title
+      FROM
+        movies
+      JOIN castings
+        ON movies.id = movie_id
+      JOIN actors
+        ON actor_id = actors.id
+      WHERE
+        name LIKE 'Art Garfunkel'
+    ) AND
+    name NOT LIKE 'Art Garfunkel'
   SQL
 end
